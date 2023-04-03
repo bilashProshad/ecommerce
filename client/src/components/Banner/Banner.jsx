3e5d1c035930962c332646b3cf18eca2727f1218
Bilash Prosad <pbilash64@gmail.com>
2023-04-03 21:30:05 +0600

    banner added

--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -29,35 +29,23 @@ const Banner = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-          <Link to="/category/63f6348bbd7ac9c66172f73d">
-            <img
-              src="https://images.pexels.com/photos/14930425/pexels-photo-14930425.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              alt=""
-            />
+          <Link to="/category/6429d5d646faac32270323b0">
+            <img src="/images/women-fashion.png" alt="" />
           </Link>
         </SwiperSlide>
         <SwiperSlide>
-          <Link to="/category/63f6316ebd7ac9c66172f738">
-            <img
-              src="https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
+          <Link to="/category/6429d5c146faac32270323ac">
+            <img src="/images/men-fashion.png" alt="" />
           </Link>
         </SwiperSlide>
         <SwiperSlide>
-          <Link to="/category/63f7cb68c3cbb0526ab547ee">
-            <img
-              src="https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              alt=""
-            />
+          <Link to="/category/6429d5d646faac32270323b0">
+            <img src="/images/women-fashion.png" alt="" />
           </Link>
         </SwiperSlide>
         <SwiperSlide>
-          <Link to="/category/63f7cc0cc3cbb0526ab547f5">
-            <img
-              src="https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              alt=""
-            />
+          <Link to="/category/6429d5e346faac32270323b4">
+            <img src="/images/laptop.png" alt="" />
           </Link>
         </SwiperSlide>
       </Swiper>
